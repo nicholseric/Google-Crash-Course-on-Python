29978bec42d7b05cb198430d32a5289f3508eb8c
Eric Nichols <nichols.m.eric@gmail.com>
2020-07-26 17:17:50 -0700

    testing conditionals:

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+def conditional_number(number):
+    if number > 11: 
+        print(0)
+    elif number != 10:
+        print(1)
+    elif number >= 20 or number < 12:
+        print(2)
+    else:
+        print(3)
+conditional_number(10)
+
